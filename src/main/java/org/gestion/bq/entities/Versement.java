70485e100ae6558b03ab31d043913afbbd43095c
yacinefbs <naouiyassine009@gmail.com>
2016-09-18 13:07:11 +0100

    Création des entités

---
+++ src/main/java/org/gestion/bq/entities/Versement.java
@@ -0,0 +1,22 @@
+package org.gestion.bq.entities;
+
+import java.util.Date;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue(value="V")
+public class Versement extends Operation {
+
+	public Versement() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Versement(Date dateOperation, double montant) {
+		super(dateOperation, montant);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
